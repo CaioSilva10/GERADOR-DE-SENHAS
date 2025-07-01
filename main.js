5060e086ddede258fc731c875449267910d4723d
CaioSilva10 <137806312+CaioSilva10@users.noreply.github.com>
2025-07-01 08:35:41 -0300

    m

--- main.js
+++ main.js
@@ -1,70 +1,79 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
-
-const botoes = document.querySelectorAll('.parametro-senha__botao');
-
-botoes[0].onclick = diminuiTamanho;
-botoes[1].onclick = aumentaTamanho;
-
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvxywz';
 const numeros = '0123456789';
 const simbolos = '!@%*?';
-
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 const campoSenha = document.querySelector('#campo-senha');
 const checkbox = document.querySelectorAll('.checkbox');
 
-for (let i = 0; i < checkbox.length; i++) {
-    checkbox[i].onclick = geraSenha;
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
+
+function diminuiTamanho(){
+
+    if (tamanhoSenha > 1){
+   // tamanhoSenha = tamanhoSenha-1;
+     tamanhoSenha--;
 }
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+function aumentaTamanho(){
+
+     if (tamanhoSenha < 20){
+   // tamanhoSenha = tamanhoSenha+1;
+     tamanhoSenha++;
+}
+    numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
+}
+
+console.log(botoes)
+
+
+for (i=0; i < checkbox.length;i++){
+  checkbox[i].onclick = geraSenha;
+}
+
+
+
 
 geraSenha();
 
-function diminuiTamanho() {
-    if (tamanhoSenha > 1) {
-        tamanhoSenha--;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}
-
-function aumentaTamanho() {
-    if (tamanhoSenha < 20) {
-        tamanhoSenha++;
-    }
-    numeroSenha.textContent = tamanhoSenha;
-    geraSenha();
-}
-
-function geraSenha() {
+function geraSenha(){
     let alfabeto = '';
-    if (checkbox[0].checked) {
-        alfabeto += letrasMaiusculas;
-    }
-    if (checkbox[1].checked) {
-        alfabeto += letrasMinusculas;
-    }
-    if (checkbox[2].checked) {
-        alfabeto += numeros;
-    }
-    if (checkbox[3].checked) {
-        alfabeto += simbolos;
-    }
-
-    // Caso nenhum checkbox esteja marcado, não gerar senha
-    if (alfabeto === '') {
-        campoSenha.value = '';
-        return;
-    }
-
-    let senha = '';
-    for (let i = 0; i < tamanhoSenha; i++) {
-        let numeroAleatorio = Math.floor(Math.random() * alfabeto.length);
-        senha += alfabeto[numeroAleatorio];
-    }
-    campoSenha.value = senha;
+if (checkbox[0].checked){
+   alfabeto = alfabeto + letrasMaiusculas;
 }
-
-// Apenas para debug
-console.log(botoes);
+if (checkbox[1].checked){
+    alfabeto = alfabeto + letrasMinusculas;
+}
+if (checkbox[2].checked){
+    alfabeto = alfabeto + numeros;
+}
+if (checkbox[3].checked){
+    alfabeto = alfabeto + simbolos;
+}
+console.log(alfabeto);
+    let senha = '';
+    for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*alfabeto.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+   senha = senha + alfabeto[numeroAleatorio];
+  }
+  campoSenha.value = senha;
+  classificaSenha();
+}
+function classificaSenha(){
+    forcaSenha.classList.remove('fraca','media','forte');
+     if (tamanhoSenha > 11){
+    forcaSenha.classList.add('forte');
+    }else {
+        forcaSenha.classList.add('media');
+    } 
+    
+}
